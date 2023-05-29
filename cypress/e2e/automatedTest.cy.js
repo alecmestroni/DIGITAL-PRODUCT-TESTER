de0fa3ec104faa2f463d8dd4497486fd09519a8a
Alec Mestroni <alec.mestroni@infocert.it>
2023-05-29 18:48:09 +0200

    fixes viewport

--- cypress/e2e/automatedTest.cy.js
+++ cypress/e2e/automatedTest.cy.js
@@ -1,5 +1,9 @@
 /// <reference types="cypress" />
-const viewports = ['iphone-x', 'samsung-s10', 'macbook-16']
+const viewports = [
+	{ device: 'iphone-xr', value: { hight: 896, width: 414 } },
+	{ device: 'macbook-16', value: { hight: 960, width: 1536 } },
+	{ device: 'samsung-s10', value: { hight: 760, width: 360 } },
+]
 const arrayOfPages = [
 	'post-insert',
 	'post-insert-images',
@@ -12,40 +16,51 @@ const url = './cypress/fixtures/url/url.html'
 
 describe('DIGITAL PRODUCT TESTER INTERVIEW', { testIsolation: false }, () => {
 	for (const viewport of viewports) {
-		describe('Automated test: "Alert mandatory registrations" (Viewport: ' + viewport + ')', { testIsolation: false }, () => {
-			it("Visit the site's home page", () => {
-				cy.visit(url)
-			})
-			it("The user clicks on the 'Publish your ad' button", () => {
-				expect(true).to.be.eq(true)
-			})
-			it('Verify that the ad publication page is displayed', () => {
-				expect(true).to.be.eq(true)
-			})
+		describe(
+			'Automated test: "Alert mandatory registrations" (Viewport: ' +
+				viewport.device +
+				' {' +
+				viewport.value.hight +
+				', ' +
+				viewport.value.width +
+				'})',
+			{ testIsolation: false, viewportHeight: viewport.value.hight, viewportWidth: viewport.value.width },
+			() => {
+				it("Visit the site's home page", () => {
+					cy.log(viewport.value.hight)
+					cy.visit(url)
+				})
+				it("The user clicks on the 'Publish your ad' button", () => {
+					expect(true).to.be.eq(true)
+				})
+				it('Verify that the ad publication page is displayed', () => {
+					expect(true).to.be.eq(true)
+				})
 
-			for (const page of arrayOfPages) {
-				it('Verify the presence of the warning in page: <./' + page + '>', () => {
-					cy.getDataCy('signUp-warning')
-				})
-				it('Verify that the warning Title in page: <./' + page + '> is spelled correctly', () => {
-					cy.getDataCy('alert-title').contains('Important Notice').should('exist')
-				})
-				it('Verify that the text of the warning in page: <./' + page + '> is spelled correctly', () => {
-					cy.getDataCy('alert-text')
-						.contains('Attention starting from day May 31, 2023 to publish your ad you will need to be registered on the site. Register Now!')
-						.should('exist')
-				})
-				it('Verify that the clickable text redirects to the correct page', () => {
-					cy.getDataCy('signUp-warning').find('[href]').click()
-					cy.url().should('include', '/registrationPage')
-					cy.wait(500)
-					cy.go('back')
-					// cy.pause()
+				for (const page of arrayOfPages) {
+					it('Verify the presence of the warning in page: <./' + page + '>', () => {
+						cy.getDataCy('signUp-warning')
+					})
+					it('Verify that the warning Title in page: <./' + page + '> is spelled correctly', () => {
+						cy.getDataCy('alert-title').contains('Important Notice').should('exist')
+					})
+					it('Verify that the text of the warning in page: <./' + page + '> is spelled correctly', () => {
+						cy.getDataCy('alert-text')
+							.contains('Attention starting from day May 31, 2023 to publish your ad you will need to be registered on the site. Register Now!')
+							.should('exist')
+					})
+					it('Verify that the clickable text redirects to the correct page', () => {
+						cy.getDataCy('signUp-warning').find('[href]').click()
+						cy.url().should('include', '/registrationPage')
+						cy.wait(500)
+						cy.go('back')
+						// cy.pause()
+					})
+				}
+				it('Ensure that the user can log out correctly after publishing the ad', () => {
+					expect(true).to.be.eq(true)
 				})
 			}
-			it('Ensure that the user can log out correctly after publishing the ad', () => {
-				expect(true).to.be.eq(true)
-			})
-		})
+		)
 	}
 })
